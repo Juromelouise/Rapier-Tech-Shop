e70f4a3c05b5a565f538519b7f06a952fbee6a08
Jurome <juromefernando@gmail.com>
2023-11-07 21:30:44 +0800

    getProducts

---
+++ backend/controllers/productController.js
@@ -0,0 +1,10 @@
+const Product = require('../models/product')
+
+exports.getProducts = async (req, res, next) => {
+	const products = await Product.find({});
+	res.status(200).json({
+		success: true,
+		count: products.length,
+		products
+	})
+}
