aa9309796dc1193a4279f2ccd5ff3b316601e8b9
Mary Jane C. Raras <maryjane.raras@tup.edu.ph>
2023-11-24 14:05:24 +0800

    Delete-Supplier

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -9,7 +9,8 @@ router.get("/products", getProducts);
 router.get("/admin/products", isAuthenticatedUser, authorizeRoles("admin"), getProducts);
 router.get("/get/supplier", getSupplier);
 router.get("/singleProduct/:id", getSingleProduct);
-router.delete('/delete/product/:id',isAuthenticatedUser, authorizeRoles('admin'), deleteProduct)
+//router.delete('/delete/product/:id',isAuthenticatedUser, authorizeRoles('admin'), deleteProduct)
+router.route('/admin/product/:id', isAuthenticatedUser, authorizeRoles('admin',)).delete(deleteProduct);
 // router.post('/new/product',isAuthenticatedUser, upload.array('images', 10), newProduct)
 router.post("/admin/new/product", isAuthenticatedUser, authorizeRoles("admin"),upload.array('images', 10), newProduct);
 router.put('/update/product/:id',isAuthenticatedUser, updateProduct)
