c4b7bf446289df4eb82f992e5190f5ff190f71a6
Jurome <juromefernando@gmail.com>
2023-11-24 22:14:04 +0800

    diko na alam ginagawa ko

--- frontend/src/Components/User/Register.js
+++ frontend/src/Components/User/Register.js
@@ -1,192 +1,192 @@
-import React, { Fragment, useState, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-import Metadata from '../Layout/Metadata'
-import axios from 'axios'
+import React, { Fragment, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Metadata from "../Layout/Metadata";
+import axios from "axios";
 
 const Register = () => {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
 
-    const [user, setUser] = useState({
-        name: '',
-        email: '',
-        password: '',
-    })
+  const { name, email, password } = user;
 
-    const { name, email, password } = user;
+  const [avatar, setAvatar] = useState("");
+  const [avatarPreview, setAvatarPreview] = useState(
+    "/images/default_avatar.jpg"
+  );
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
-    const [avatar, setAvatar] = useState('')
-    const [avatarPreview, setAvatarPreview] = useState('/images/default_avatar.jpg')
-    const [isAuthenticated, setIsAuthenticated] = useState(false)
-    const [error, setError] = useState('')
-    const [loading, setLoading] = useState(true)
-   
-
-    // let navigate = useNavigate()
-    // useEffect(() => {
-    //     if (isAuthenticated) {
-    //         navigate('/')
-    //     }
-    //     if (error) {
-    //         console.log(error)
-    //        setError()
-    //     }
-
-    // }, [error. isAuthenticated, navigate])
-
-    const submitHandler = (e) => {
-        e.preventDefault();
-
-        const formData = new FormData();
-        formData.set('name', name);
-        formData.set('email', email);
-        formData.set('password', password);
-        formData.set('avatar', avatar);
-
-        register(formData)
+  let navigate = useNavigate();
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate("/");
     }
-
-    const onChange = e => {
-        if (e.target.name === 'avatar') {
-
-            const reader = new FileReader();
-
-            reader.onload = () => {
-                if (reader.readyState === 2) {
-                    setAvatarPreview(reader.result)
-                    setAvatar(reader.result)
-                }
-            }
-
-            reader.readAsDataURL(e.target.files[0])
-
-        } else {
-            setUser({ ...user, [e.target.name]: e.target.value })
-        }
+    if (error) {
+      console.log(error);
+      setError((prevError) => error.response.data.success);
+      setLoading(false);
     }
-    const register = async (userData) => {
-        try {
-            const config = {
-                headers: {
-                    'Content-Type': 'multipart/form-data'
-                }
-            }
-    
-            const response = await axios.post(`http://localhost:4001/api/v1/register`, userData, config);
-    
-            if (response && response.data && response.data.user) {
-                console.log(response.data.user);
-                setUser(response.data.user);
-                // Handle other success-related actions if needed
-            } else {
-                // Handle the case where the response does not have the expected structure
-                console.log("Unexpected response format:", response);
-            }
-        } catch (error) {
-            setIsAuthenticated(false);
-            setLoading(false);
-            setUser(null);
-    
-            // Check if the error has a response property and a data property
-            if (error.response && error.response.data && error.response.data.message) {
-                setError(error.response.data.message);
-                console.log(error.response.data.message);
-            } else {
-                // Handle unexpected errors
-                console.error("Unexpected error:", error);
-            }
+  }, [error, isAuthenticated, navigate]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.set("name", name);
+    formData.set("email", email);
+    formData.set("password", password);
+    formData.set("avatar", avatar);
+
+    setLoading(true); // Add this line to handle loading state
+    register(formData);
+  };
+
+  const onChange = (e) => {
+    if (e.target.name === "avatar") {
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        if (reader.readyState === 2) {
+          setAvatarPreview(reader.result);
+          setAvatar(reader.result);
         }
-    };
-    
+      };
 
+      reader.readAsDataURL(e.target.files[0]);
+    } else {
+      setUser({ ...user, [e.target.name]: e.target.value });
+    }
+  };
+  const register = async (userData) => {
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      };
 
-    return (
-        <Fragment>
+      const response = await axios.post(
+        `${process.env.REACT_APP_API}/api/v1/register`,
+        userData,
+        config
+      );
 
-            <Metadata title={'Register User'} />
-            {/* <title>Register User</title> */}
+      if (response && response.data && response.data.user) {
+        // setSuccess(true);
+      } else {
+        console.log("Unexpected response format:", response);
+      }
+    } catch (error) {
+      setIsAuthenticated(false);
+      setLoading(false);
+      if (
+        error.response &&
+        error.response.data &&
+        error.response.data.success
+      ) {
+        setError((prevError) => error.response.data.success);
+        console.log(error.response.data.success);
+      } else {
+        console.error("Unexpected error:", error);
+      }
+    }
+  };
 
-            <div className="row wrapper">
-                <div className="col-10 col-lg-5">
-                    <form className="shadow-lg" onSubmit={submitHandler} encType='multipart/form-data'>
-                        <h1 className="mb-3">Register</h1>
+  return (
+    <Fragment>
+      <Metadata title={"Register User"} />
+      {/* <title>Register User</title> */}
 
-                        <div className="form-group">
-                            <label htmlFor="email_field">Name</label>
-                            <input
-                                type="name"
-                                id="name_field"
-                                className="form-control"
-                                name='name'
-                                value={name}
-                                onChange={onChange}
-                            />
-                        </div>
+      <div className="row wrapper">
+        <div className="col-10 col-lg-5">
+          <form
+            className="shadow-lg"
+            onSubmit={submitHandler}
+            encType="multipart/form-data"
+          >
+            <h1 className="mb-3">Register</h1>
 
-                        <div className="form-group">
-                            <label htmlFor="email_field">Email</label>
-                            <input
-                                type="email"
-                                id="email_field"
-                                className="form-control"
-                                name='email'
-                                value={email}
-                                onChange={onChange}
-                            />
-                        </div>
-
-                        <div className="form-group">
-                            <label htmlFor="password_field">Password</label>
-                            <input
-                                type="password"
-                                id="password_field"
-                                className="form-control"
-                                name='password'
-                                value={password}
-                                onChange={onChange}
-                            />
-                        </div>
-
-                        <div className='form-group'>
-                            <label htmlFor='avatar_upload'>Avatar</label>
-                            <div className='d-flex align-items-center'>
-                                <div>
-                                    <figure className='avatar mr-3 item-rtl'>
-                                        <img
-                                            src={avatarPreview}
-                                            className='rounded-circle'
-                                            alt='Avatar Preview'
-                                        />
-                                    </figure>
-                                </div>
-                                <div className='custom-file'>
-                                    <input
-                                        type='file'
-                                        name='avatar'
-                                        className='custom-file-input'
-                                        id='customFile'
-                                        accept="images/*"
-                                        onChange={onChange}
-                                    />
-                                    <label className='custom-file-label' htmlFor='customFile'>
-                                        Choose Avatar
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <button
-                            id="register_button"
-                            type="submit"
-                            className="btn btn-block py-3"
-                            // disabled={loading ? false : true}
-                        >
-                            REGISTER
-                        </button>
-                    </form>
-                </div>
+            <div className="form-group">
+              <label htmlFor="email_field">Name</label>
+              <input
+                type="name"
+                id="name_field"
+                className="form-control"
+                name="name"
+                value={name}
+                onChange={onChange}
+              />
             </div>
 
-        </Fragment>
-    )
-}
+            <div className="form-group">
+              <label htmlFor="email_field">Email</label>
+              <input
+                type="email"
+                id="email_field"
+                className="form-control"
+                name="email"
+                value={email}
+                onChange={onChange}
+              />
+            </div>
 
-export default Register
+            <div className="form-group">
+              <label htmlFor="password_field">Password</label>
+              <input
+                type="password"
+                id="password_field"
+                className="form-control"
+                name="password"
+                value={password}
+                onChange={onChange}
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="avatar_upload">Avatar</label>
+              <div className="d-flex align-items-center">
+                <div>
+                  <figure className="avatar mr-3 item-rtl">
+                    <img
+                      src={avatarPreview}
+                      className="rounded-circle"
+                      alt="Avatar Preview"
+                    />
+                  </figure>
+                </div>
+                <div className="custom-file">
+                  <input
+                    type="file"
+                    name="avatar"
+                    className="custom-file-input"
+                    id="customFile"
+                    accept="images/*"
+                    onChange={onChange}
+                  />
+                  <label className="custom-file-label" htmlFor="customFile">
+                    Choose Avatar
+                  </label>
+                </div>
+              </div>
+            </div>
+
+            <button
+              id="register_button"
+              type="submit"
+              className="btn btn-block py-3"
+              disabled={loading}
+            >
+              REGISTER
+            </button>
+          </form>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Register;
