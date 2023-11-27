afc7da9534f49c05fd6d0b9af95a7fdf5ac73f3f
DyNis999 <130058569+DyNis999@users.noreply.github.com>
2023-11-27 15:16:39 +0800

    SupAd-Vali

--- frontend/src/Components/Admin/NewSupplier.js
+++ frontend/src/Components/Admin/NewSupplier.js
@@ -6,34 +6,44 @@ import { getToken } from "../../utils/helpers";
 import axios from "axios";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string().required("Name is required"),
+  number: Yup.string().required("Phone Number is required"),
+  address: Yup.string().required("Address is required"),
+});
 
 const NewSupplier = () => {
-  const [name, setName] = useState("");
-  const [number, setNumber] = useState("");
-  const [address, setAddress] = useState("");
   const [images, setImage] = useState([]);
   const [imagesPreview, setImagesPreview] = useState([]);
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(true);
   const [success, setSuccess] = useState("");
   const [supplier, setSupplier] = useState({});
-
   let navigate = useNavigate();
 
-  const submitHandler = (e) => {
-    e.preventDefault();
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      number: "",
+      address: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      const formData = new FormData();
+      formData.set("name", values.name);
+      formData.set("number", values.number);
+      formData.set("address", values.address);
 
-    const formData = new FormData();
-    formData.set("name", name);
-    formData.set("number", number);
-    formData.set("address", address);
+      images.forEach((image) => {
+        formData.append("images", image);
+      });
 
-    images.forEach((image) => {
-      formData.append("images", image);
-    });
-
-    newSupplier(formData);
-  };
+      newSupplier(formData);
+    },
+  });
 
   const onChange = (e) => {
     const files = Array.from(e.target.files);
@@ -49,9 +59,9 @@ const NewSupplier = () => {
       };
 
       reader.readAsDataURL(file);
-      // console.log(reader)
     });
   };
+
   const newSupplier = async (formData) => {
     try {
       const config = {
@@ -73,6 +83,7 @@ const NewSupplier = () => {
       setError(error.message);
     }
   };
+
   useEffect(() => {
     if (error) {
       toast.error(error, {
@@ -92,16 +103,14 @@ const NewSupplier = () => {
     <Fragment>
       <MetaData title={"New Supplier"} />
       <div className="row">
-        <div className="col-12 col-md-2">
-          {/* <Sidebar /> */}
-        </div>
+        <div className="col-12 col-md-2"></div>
 
         <div className="col-12 col-md-10">
           <Fragment>
             <div className="wrapper my-5">
               <form
                 className="shadow-lg"
-                onSubmit={submitHandler}
+                onSubmit={formik.handleSubmit}
                 encType="multipart/form-data"
               >
                 <h1 className="mb-4">Add Supplier</h1>
@@ -112,20 +121,24 @@ const NewSupplier = () => {
                     type="text"
                     id="name_field"
                     className="form-control"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
+                    {...formik.getFieldProps("name")}
                   />
+                  {formik.touched.name && formik.errors.name && (
+                    <div className="text-danger">{formik.errors.name}</div>
+                  )}
                 </div>
 
                 <div className="form-group">
                   <label htmlFor="number_field">Phone Number</label>
                   <input
-                    type="number"
+                    type="text"
                     id="number_field"
                     className="form-control"
-                    value={number}
-                    onChange={(e) => setNumber(e.target.value)}
+                    {...formik.getFieldProps("number")}
                   />
+                  {formik.touched.number && formik.errors.number && (
+                    <div className="text-danger">{formik.errors.number}</div>
+                  )}
                 </div>
 
                 <div className="form-group">
@@ -134,13 +147,15 @@ const NewSupplier = () => {
                     className="form-control"
                     id="address_field"
                     rows="8"
-                    value={address}
-                    onChange={(e) => setAddress(e.target.value)}
+                    {...formik.getFieldProps("address")}
                   ></textarea>
+                  {formik.touched.address && formik.errors.address && (
+                    <div className="text-danger">{formik.errors.address}</div>
+                  )}
                 </div>
+
                 <div className="form-group">
                   <label>Images</label>
-
                   <div className="custom-file">
                     <input
                       type="file"
@@ -154,7 +169,6 @@ const NewSupplier = () => {
                       Choose Images
                     </label>
                   </div>
-
                   {imagesPreview.map((img) => (
                     <img
                       src={img}
@@ -166,11 +180,11 @@ const NewSupplier = () => {
                     />
                   ))}
                 </div>
+
                 <button
                   id="login_button"
                   type="submit"
                   className="btn btn-block py-3"
-                  // disabled={loading ? true : false}
                 >
                   CREATE
                 </button>
@@ -182,4 +196,5 @@ const NewSupplier = () => {
     </Fragment>
   );
 };
+
 export default NewSupplier;
