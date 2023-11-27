35f34d074c0a992d986c9c9e9f2b89f914ac9453
DyNis999 <130058569+DyNis999@users.noreply.github.com>
2023-11-27 13:02:47 +0800

    Val-Prod-Up

--- frontend/src/Components/Layout/Header.js
+++ frontend/src/Components/Layout/Header.js
@@ -30,12 +30,14 @@ const Header = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
+        {/* Logo on the left */}
         <Link to="/" style={{ textDecoration: "none", fontWeight: "400" }}>
           <div className="navbar-brand">
             <img id="LogoHeader" alt="Logo" src="../images/Logo.PNG" />
-            <strong>Rapier Tech Shop</strong>
           </div>
         </Link>
+
+        {/* Toggle button for small screens */}
         <button
           className="navbar-toggler"
           type="button"
@@ -48,68 +50,73 @@ const Header = () => {
           <span className="navbar-toggler-icon"></span>
         </button>
 
+        {/* Content aligned to the right */}
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              {user ? (
-                <div className="dropdown">
-                  <button
-                    className="btn btn-secondary dropdown-toggle"
-                    type="button"
-                    id="dropDownMenuButton"
-                    data-bs-toggle="dropdown"
-                    aria-haspopup="true"
-                    aria-expanded="false"
-                  >
-                    <figure className="avatar avatar-nav">
-                      <img
-                        src={user.avatar && user.avatar.url}
-                        alt={user && user.name}
-                        className="rounded-circle"
-                      />
-                    </figure>
-                    <span>{user && user.name}</span>
-                  </button>
-                  <div
-                    className="dropdown-menu"
-                    aria-labelledby="dropDownMenuButton"
-                  >
-                    {user && user.role === "admin" && (
-                      <Link className="dropdown-item" to="/admin/dashboard">
-                        Dashboard
-                      </Link>
-                    )}
-                    <Link className="dropdown-item" to="/me">
-                      Profile
-                    </Link>
-                    <Link
-                      className="dropdown-item text-danger"
-                      to="/"
-                      onClick={logoutUser}
-                    >
-                      Logout
-                    </Link>
-                  </div>
-                </div>
-              ) : (
-                <Link to="/login" className="btn ml-4" id="login_btn">
-                  Login
-                </Link>
-              )}
-            </li>
-          </ul>
+          <div className="ms-auto d-flex align-items-center">
+            <div className="Cartitem">
+              <Link to="/cart">
+                <i className="bi bi-cart"></i>
+              </Link>
+            </div>
 
-          <div className="Cartitem">
-            <Link to="/cart">
-              <i className="bi bi-cart"></i>
-            </Link>
+            <Search />
+
+            <div className="AangAvatar">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  {user ? (
+                    <div className="dropdown">
+                      <button
+                        className="btn btn-secondary dropdown-toggle"
+                        type="button"
+                        id="dropDownMenuButton"
+                        data-bs-toggle="dropdown"
+                        aria-haspopup="true"
+                        aria-expanded="false"
+                      >
+                        <figure className="avatar avatar-nav">
+                          <img
+                            src={user.avatar && user.avatar.url}
+                            alt={user && user.name}
+                            className="rounded-circle"
+                          />
+                        </figure>
+                        <span>{user && user.name}</span>
+                      </button>
+                      <div
+                        className="dropdown-menu"
+                        aria-labelledby="dropDownMenuButton"
+                      >
+                        {user && user.role === "admin" && (
+                          <Link className="dropdown-item" to="/admin/dashboard">
+                            Dashboard
+                          </Link>
+                        )}
+                        <Link className="dropdown-item" to="/me">
+                          Profile
+                        </Link>
+                        <Link
+                          className="dropdown-item text-danger"
+                          to="/"
+                          onClick={logoutUser}
+                        >
+                          Logout
+                        </Link>
+                      </div>
+                    </div>
+                  ) : (
+                    <Link to="/login" className="btn ml-4" id="login_btn">
+                      Login
+                    </Link>
+                  )}
+                </li>
+              </ul>
+            </div>
           </div>
-
-          <Search />
         </div>
       </div>
     </nav>
   );
-};
+}
 
 export default Header;
